d543725dc59add9b3f6fe55c1317b57b88e21ead
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2020-05-01 10:52:43 +0200

    Bum squeezenfc package

--- br_external/package/squeezenfc/squeezenfc.mk
+++ br_external/package/squeezenfc/squeezenfc.mk
@@ -3,7 +3,7 @@
 # Squeezenfc
 #
 ################################################################################
-SQUEEZENFC_VERSION = 8f7925cec87f693e5a670785af152d6dd9f8ae58
+SQUEEZENFC_VERSION = 207b27f7bbe42aa276d48005a455de2762f88b00
 SQUEEZENFC_SITE = $(call github,naguirre,squeezenfc,$(SQUEEZENFC_VERSION))
 SQUEEZENFC_LICENSE = GPLv3
 SQUEEZENFC_LICENSE_FILES = COPYING
@@ -25,6 +25,8 @@ endef
 
 define SQUEEZENFC_INSTALL_TARGET_CMDS
 $(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) ninja $(SQUEEZENFC_NINJA_OPTS) -C $(@D)/build install
+mkdir -p $(TARGET_DIR)/usr/share/squeezenfc/
+cp $(@D)/ding.wav $(TARGET_DIR)/usr/share/squeezenfc/
 rm -rf $(TARGET_DIR)/usr/include
 endef
 
